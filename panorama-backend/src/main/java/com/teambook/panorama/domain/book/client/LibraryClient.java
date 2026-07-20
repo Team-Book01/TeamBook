4d47f6552b49f66f3de6513afe81bc71669e5b46
sven-kor <164502736+sven-kor@users.noreply.github.com>
2026-07-20 21:29:17 +0900

    feat: 정보나루 api 쿼리 파라미터 수정, 타임아웃 설정, 페이징 처리

--- panorama-backend/src/main/java/com/teambook/panorama/domain/book/client/LibraryClient.java
+++ panorama-backend/src/main/java/com/teambook/panorama/domain/book/client/LibraryClient.java
@@ -1,12 +1,15 @@
 package com.teambook.panorama.domain.book.client;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
 
-import com.teambook.panorama.domain.book.dto.library.LibBookAvailability;
-import com.teambook.panorama.domain.book.dto.library.LibSrchResponse;
+import com.teambook.panorama.domain.book.dto.library.BookAvResponseWrapper;
+
+import com.teambook.panorama.domain.book.dto.library.LibSrchResponseWrapper;
 import com.teambook.panorama.global.exception.BusinessException;
 import com.teambook.panorama.global.exception.ErrorCode;
 
@@ -22,12 +25,21 @@ public class LibraryClient {
   private final RestClient libraryRestClient;
 
   //도서 소장 도서관 조회
-  public LibSrchResponse searchLib(String isbn, String regionCode) {
+  public LibSrchResponseWrapper searchLib(String isbn, String regionCode, String dtlRegion, Integer pageNo, Integer pageSize) {
     try {
       return libraryRestClient.get()
-    .uri("/libSrchByBook?authKey={a}&isbn={i}&region={r}&format=json", this.authKey, isbn, regionCode)
+    .uri(uriBuilder -> uriBuilder
+      .path("/libSrchByBook")
+      .queryParam("authKey", this.authKey)
+      .queryParam("isbn", isbn)
+      .queryParam("region", regionCode)
+      .queryParamIfPresent("dtl_region", Optional.ofNullable(dtlRegion))
+      .queryParam("pageNo", Optional.ofNullable(pageNo))
+      .queryParam("pageSize", Optional.ofNullable(pageSize))
+      .queryParam("format", "json")
+      .build())
     .retrieve()
-    .body(LibSrchResponse.class);
+    .body(LibSrchResponseWrapper.class);
     } catch (RestClientException e) {
       throw new BusinessException(ErrorCode.LIBRARY_API_ERROR);
     }
@@ -35,14 +47,14 @@ public class LibraryClient {
   }
   //해당 대출 가능 여부 파악
   // 파라미터명은 isbn 이 아니라 isbn13 이다(isbn 으로 보내면 isbnLengthErr).
-  public LibBookAvailability checkBookAvailability(String libCode, String isbn){
+  public BookAvResponseWrapper checkBookAvailability(String libCode, String isbn){
     try {
       return libraryRestClient.get()
     .uri("/bookExist?authKey={a}&libCode={l}&isbn13={i}&format=json", this.authKey, libCode, isbn)
     .retrieve()
-    .body(LibBookAvailability.class);
+    .body(BookAvResponseWrapper.class);
     } catch (RestClientException e) {
-      throw new BusinessException(ErrorCode.LIBRARY_API_ERROR);
+      return null;
     }
 
   }
