2555af8fb6eb0e00567d5411fa11f903b040f1b3
sven-kor <164502736+sven-kor@users.noreply.github.com>
2026-07-20 21:33:37 +0900

    fix: null 방어 및 예외처리 보강

--- panorama-backend/src/main/java/com/teambook/panorama/domain/book/service/LibraryService.java
+++ panorama-backend/src/main/java/com/teambook/panorama/domain/book/service/LibraryService.java
@@ -8,11 +8,15 @@ import java.util.Map;
 import org.springframework.stereotype.Service;
 
 import com.teambook.panorama.domain.book.client.LibraryClient;
-import com.teambook.panorama.domain.book.dto.library.LibBookAvailability;
+import com.teambook.panorama.domain.book.dto.library.BookAvResponseWrapper;
+import com.teambook.panorama.domain.book.dto.library.BookAvailabilityResult;
 import com.teambook.panorama.domain.book.dto.library.LibSrchResponse;
-import com.teambook.panorama.domain.book.dto.library.LibWrapper;
+import com.teambook.panorama.domain.book.dto.library.LibSrchResponseWrapper;
+import com.teambook.panorama.domain.book.dto.library.LibSrchItemWrapper;
 import com.teambook.panorama.domain.book.dto.library.LibraryListResponse;
 import com.teambook.panorama.domain.book.dto.library.LibraryResponseItem;
+import com.teambook.panorama.global.exception.BusinessException;
+import com.teambook.panorama.global.exception.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,28 +26,39 @@ public class LibraryService {
 
   private final LibraryClient libraryClient;
 
-  public LibraryListResponse findLibAndBook(String isbn, String regionCode){
+  public LibraryListResponse findLibAndBook(String isbn, String regionCode, String dtlRegion, Integer pageNo, Integer pageSize){
 
-    //소장 도서관 목록
-    LibSrchResponse libSrchResponse = libraryClient.searchLib(isbn, regionCode);
-    if (libSrchResponse == null || libSrchResponse.resultNum() == 0) {
+    //첫번째 겹 호출
+    LibSrchResponseWrapper apiResponse = libraryClient.searchLib(isbn, regionCode, dtlRegion, pageNo, pageSize);
+    if (apiResponse == null || apiResponse.response() == null) {
+    throw new BusinessException(ErrorCode.LIBRARY_API_ERROR);
+}
+    LibSrchResponse libSrchResponse = apiResponse.response();
+    //null 방어
+    if (libSrchResponse.numFound() == null || libSrchResponse.resultNum() == null) {
+      throw new BusinessException(ErrorCode.LIBRARY_API_ERROR);
+    }
+    if (libSrchResponse.numFound() == 0 || libSrchResponse.libs() == null) {
       return new LibraryListResponse(0, List.of());
     }
     //도서관 수
-    int total = libSrchResponse.resultNum();
+    int total = libSrchResponse.numFound();
     
     //도서관 코드 목록
     List<String> libCodes = libSrchResponse.libs().stream().map(wrapper -> wrapper.lib().libCode()).toList();
 
     //대출 가능 여부 조회 -> 도서관 코드와 맵으로 만들기
-    Map<String, LibBookAvailability> bookAvMap = new HashMap<>();
+    Map<String, BookAvailabilityResult> bookAvMap = new HashMap<>();
     for (String libCode : libCodes) {
-      var libBookAvailability = libraryClient.checkBookAvailability(libCode, isbn);
-      bookAvMap.put(libCode, libBookAvailability);
+      BookAvResponseWrapper bookAvResponseWrapper =  libraryClient.checkBookAvailability(libCode, isbn);
+      BookAvailabilityResult result = extraResult(bookAvResponseWrapper);
+      //result 값도 null일 수 있음.
+      bookAvMap.put(libCode, result);
     }
 
     //도서관 리스트
-    var list = libSrchResponse.libs().stream().map(LibWrapper::lib).map(lib -> {
+    var list = libSrchResponse.libs().stream().map(LibSrchItemWrapper::lib).map(lib -> {
+      BookAvailabilityResult result = bookAvMap.get(lib.libCode());
       return
       LibraryResponseItem.builder()
       .address(lib.address())
@@ -52,8 +67,8 @@ public class LibraryService {
       .libName(lib.libName())
       .operatingTime(lib.operatingTime())
       .tel(lib.tel())
-      .hasBook(toBoolean(bookAvMap.get(lib.libCode()).hasBook()))
-      .loanAvailable(toBoolean(bookAvMap.get(lib.libCode()).loanAvailable()))
+      .hasBook(result != null ? toBoolean(result.hasBook()) : null)
+      .loanAvailable(result != null ? toBoolean(result.loanAvailable()) : null)
       .build();
     }).toList();
     return
@@ -63,6 +78,14 @@ public class LibraryService {
   private Boolean toBoolean(String value){
     if (value == null) return null;
     return value.equals("Y");
-
   }
+  //null 방어 / 에러 처리
+  private BookAvailabilityResult extraResult(BookAvResponseWrapper responseWrapper) {
+    if(responseWrapper == null || responseWrapper.response() == null) return null;
+    if ("authErr".equals(responseWrapper.response().errCode())) throw new BusinessException(ErrorCode.LIBRARY_API_ERROR);
+    if (responseWrapper.response().result() == null) 
+      return null;
+    return responseWrapper.response().result();
+  }
+  
 }
