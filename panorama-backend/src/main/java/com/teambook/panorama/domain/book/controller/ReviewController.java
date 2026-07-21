ff343cc6dc91a2f39e52d48bd3554a4a9a5107ad
sven-kor <164502736+sven-kor@users.noreply.github.com>
2026-07-22 01:17:57 +0900

    chore: 불필요한 import삭제

--- panorama-backend/src/main/java/com/teambook/panorama/domain/book/controller/ReviewController.java
+++ panorama-backend/src/main/java/com/teambook/panorama/domain/book/controller/ReviewController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.teambook.panorama.domain.book.dto.review.MyReviewItem;
 import com.teambook.panorama.domain.book.dto.review.MyReviewResponse;
 import com.teambook.panorama.domain.book.dto.review.ReviewItem;
 import com.teambook.panorama.domain.book.dto.review.ReviewRequest;
